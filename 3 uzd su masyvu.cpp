ab47882fddaab0e214072c71d7e187a21d31295d
j.kunciene@gmail.com <48964713+jkunciene@users.noreply.github.com>
2019-10-21 18:54:31 +0300

    parsinesiau suma, radau vidutine per diena

--- 3 uzd su masyvu.cpp
+++ 3 uzd su masyvu.cpp
@@ -10,22 +10,28 @@ ofstream irasymas("Rezultatai.txt"); //komanda duomenu rasymui
 void nuskaitymasIsFailo(int &dienuSkaicius, double &SimoFondas, double &LinoFondas, double &MantoFondas, double SimoMasyvas[10], double LinoMasyvas[10], double MantoMasyvas[10]);
 void FailoIrasymas(double SimoMasyvas[10], double LinoMasyvas[10], double MantoMasyvas[10]);
 double piniguIsleidimui(double M[10]);
-//------kaip paskaiciuoti skirtuma, jei nemoku prisiskirti griztancios sumos is funkcijos rasyciau funkcija returninancia skirtuma pvz: double KiekLiko(VaikinoSuma, VaikinoFondas);
+double VidutDienos(double M[10], int dienuSkaicius);
 
 
 int main() {
 
     int dienuSkaicius;
-    double SimoFondas, LinoFondas, MantoFondas, SimoMasyvas[10], LinoMasyvas[10], MantoMasyvas[10], M[10];
+    double SimoFondas, SimoSum, SimasperDiena, LinoFondas, LinoSum, LinasperDiena, MantoFondas, MantoSum, MantasperDiena, SimoMasyvas[10], LinoMasyvas[10], MantoMasyvas[10], M[10];
 
     nuskaitymasIsFailo(dienuSkaicius, SimoFondas, LinoFondas, MantoFondas, SimoMasyvas, LinoMasyvas, MantoMasyvas);
 
     cout<<dienuSkaicius<<"   "<<SimoFondas<<"   "<<LinoFondas<<"   "<<MantoFondas<<endl;
 
     //-------kodel negalima buvo uzrasyti SimoSuma = piniguIsleidimui(SimoMasyvas); ir tt klaida incompatible types in assignment of double to double
+    SimoSum=piniguIsleidimui(SimoMasyvas);
+    LinoSum=piniguIsleidimui(LinoMasyvas);
+    MantoSum=piniguIsleidimui(MantoMasyvas);
+    cout<<"Simas isleido  "<<SimoSum<<"   Linas isleido  "<<LinoSum<<"   Mantas isleido  "<<MantoSum<<endl;
 
-    cout<<"Simas isleido  "<<piniguIsleidimui(SimoMasyvas)<<"   Linas isleido  "<<piniguIsleidimui(LinoMasyvas)<<"   Mantas isleido  "<<piniguIsleidimui(MantoMasyvas)<<endl;
-
+    SimasperDiena=VidutDienos(SimoMasyvas, dienuSkaicius);
+    LinasperDiena=VidutDienos(LinoMasyvas, dienuSkaicius);
+    MantasperDiena=VidutDienos(MantoMasyvas, dienuSkaicius);
+    cout<<"Simas isleidzia vidutiniskai per diena   "<<SimasperDiena<<"  Linas isleidzia vidutiniskai per diena   "<<LinasperDiena<<"  Mantas isleidzia vidutiniskai per diena   "<<MantasperDiena;
     FailoIrasymas(SimoMasyvas, LinoMasyvas, MantoMasyvas);
 
 
@@ -55,4 +61,11 @@ void FailoIrasymas(double SimoMasyvas[10], double LinoMasyvas[10], double MantoM
             }
     irasymas<<"Simas isleido  "<<piniguIsleidimui(SimoMasyvas)<<"    Linas isleido  "<<piniguIsleidimui(LinoMasyvas)<<"   Mantas isleido  "<<piniguIsleidimui(MantoMasyvas)<<endl;
 }
-
+double VidutDienos(double M[10], int dienuSkaicius){
+    double suma;
+    suma=0;
+    for(int i=0; i<10; i++){
+        suma= suma + M[i];
+    }
+    return(suma/dienuSkaicius);
+}
